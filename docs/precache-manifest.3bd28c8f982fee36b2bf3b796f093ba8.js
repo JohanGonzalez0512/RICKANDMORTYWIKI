0244afc98adf3bb5f18b7839c48a39bc4f373bcb
Johan Gonzalez <Jjopi20101@gmail.com>
2020-09-10 20:05:49 -0500

    docs

---
+++ docs/precache-manifest.3bd28c8f982fee36b2bf3b796f093ba8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77d17fb4614291c34b756a2a4d86d2a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "578673d75d484a113204",
+    "url": "/static/js/2.77b5a72d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.77b5a72d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c619c8234a38f0950f0e",
+    "url": "/static/js/main.cd0981c2.chunk.js"
+  },
+  {
+    "revision": "ea4d14359a914f65f76f",
+    "url": "/static/js/runtime-main.1fbd70cc.js"
+  }
+]);
